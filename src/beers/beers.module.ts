fc9e8df69d31a6c9de8fdaba893fbce42c4bb40c
Facundo Lezcano <facu.lezca@gmail.com>
2022-03-02 13:50:38 -0300

    Adds validation

--- src/beers/beers.module.ts
+++ src/beers/beers.module.ts
@@ -1,9 +1,12 @@
 import { Module } from '@nestjs/common';
 import { BeersService } from './beers.service';
 import { BeersController } from './beers.controller';
+import { MongooseModule } from "@nestjs/mongoose";
+import { Beer, BeerSchema } from "./schemas/beer.schema";
 
 @Module({
+  imports: [MongooseModule.forFeature([{ name: Beer.name, schema: BeerSchema }])],
   controllers: [BeersController],
-  providers: [BeersService]
+  providers: [BeersService],
 })
 export class BeersModule {}
--- src/beers/dto/create-beer.dto.ts
+++ src/beers/dto/create-beer.dto.ts
@@ -1,4 +1,9 @@
+import { IsArray, IsNotEmpty } from "class-validator";
+
 export class CreateBeerDto {
+  @IsNotEmpty()
   name: string;
+
+  @IsArray()
   ingredients: Array<Object>
 }
